c444ee3e93f82fe355ea6e254a1c6d5bf69f0bc2
Luke Parker <moongateuk@gmail.com>
2016-02-24 11:08:25 +0000

    Setup IPlayerRepository

--- src/data-access/PlayerRepo/IPlayerRepository.cs
+++ src/data-access/PlayerRepo/IPlayerRepository.cs
@@ -9,6 +9,11 @@ namespace data_access.PlayerRepo
 {
     public interface IPlayerRepository
     {
-        
+        Player Add(string username, string displayName,string email, string passwordHash);
+        Player Get(string username);
+        Player GetAll(bool onlyActive);
+        void Deactivate(string username, string email, string passwordHash);
+        void Activate(string username, string email, Guid token);
+        void Delete(string username, string email, string passwordHash);
     }
 }
