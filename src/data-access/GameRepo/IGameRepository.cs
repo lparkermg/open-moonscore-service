dd2b9b338d5dff27c87c69cfaf795869327ecaac
Luke Parker <moongateuk@gmail.com>
2016-02-24 12:16:19 +0000

    Populated IGameRepository.

--- src/data-access/GameRepo/IGameRepository.cs
+++ src/data-access/GameRepo/IGameRepository.cs
@@ -3,10 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using entities;
 
 namespace data_access.GameRepo
 {
     public interface IGameRepository
     {
+        void Add(Game game);
+        Game Get(Guid id);
+        void Activate(Guid id);
+        void Deactivate(Guid id);
+        void Delete(Guid id);
     }
 }
